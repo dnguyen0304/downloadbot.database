6259c373fba0f7d4b22efc4413e82a16cabcf95a
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-12 12:12:44 -0500

    ME-729: Added Replay repository.

--- downloadbot_cache/repositories.py
+++ downloadbot_cache/repositories.py
@@ -2,6 +2,8 @@
 
 import abc
 
+import redis
+
 
 class Repository(metaclass=abc.ABCMeta):
 
@@ -25,3 +27,48 @@ class Repository(metaclass=abc.ABCMeta):
         """
 
         raise NotImplementedError
+
+
+class Replay(Repository):
+
+    def __init__(self, client, marshaller):
+
+        """
+        Implementation backed by the redis library.
+
+        Parameters
+        ----------
+        client : redis.client.StrictRedis
+        marshaller : downloadbot_cache.marshallers.ReplayToMap
+        """
+
+        self._client = client
+        self._marshaller = marshaller
+
+    def add(self, model):
+
+        """
+        The time complexity is O(n), where n is the number of fields
+        being set.
+        """
+
+        # This should not use string literals.
+        # This should catch redis.exceptions.ConnectionError and
+        # redis.exceptions.TimeoutError exceptions. These exceptions
+        # are not documented.
+
+        # This operation is atomic.
+        next_replay_id = self._client.incr('replays:id:next')
+        model.replays_id = int(next_replay_id)
+        mapping = self._marshaller.marshall(source=model)
+        try:
+            self._client.hmset('replays:' + next_replay_id, mapping=mapping)
+        except redis.DataError:
+            # An expected case has occurred. The map contains no items.
+            pass
+
+    def __repr__(self):
+        repr_ = '{}(client={}, marshaller={})'
+        return repr_.format(self.__class__.__name__,
+                            self._client,
+                            self._marshaller)
