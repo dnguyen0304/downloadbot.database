d88a26e9c43060a4e958ca7f0fc5fd5d8864b0fb
Duy Nguyen <dnguyen0304@gmail.com>
2017-11-15 12:38:00 -0500

    ME-729: Changed runtime base not to create a configuration directory.

--- scripts/build-runtime-base.sh
+++ scripts/build-runtime-base.sh
@@ -4,9 +4,13 @@ set -eu
 
 PACKAGE_NAME=$(./scripts/get-package-name.sh)
 TAG=$(./scripts/get-package-version.sh)
+CONFIGURATION_DIRECTORY_NAME=$(./scripts/get-configuration-directory-name.sh)
+CONFIGURATION_FILE_NAME=$(./scripts/get-configuration-file-name.sh)
 
 docker build \
     --file docker/runtime/base/Dockerfile \
     --tag dnguyen0304/${PACKAGE_NAME}-runtime-base:${TAG} \
     --build-arg PACKAGE_NAME=${PACKAGE_NAME} \
+    --build-arg CONFIGURATION_DIRECTORY_NAME=${CONFIGURATION_DIRECTORY_NAME} \
+    --build-arg CONFIGURATION_FILE_NAME=${CONFIGURATION_FILE_NAME} \
     .
